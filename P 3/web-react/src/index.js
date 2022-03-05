fdf72728d42d06f0e64dd4d50df367a492bde532
fauzanmuh <f4uz4n.oioi@gmail.com>
2022-03-05 13:39:36 +0700

    Finish Praktikum Modul 3

--- P 3/web-react/src/index.js
+++ P 3/web-react/src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import LifeCycle from './LifeCycle';
+import HelloComponent from './component/HelloComponent';
 import reportWebVitals from './reportWebVitals';
-
-function HelloComponent() {
-  return <p>HelloComponent</p> 
-}
 
 class stateFullComponent extends React.Component {
   render() {
@@ -14,7 +12,7 @@ class stateFullComponent extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
