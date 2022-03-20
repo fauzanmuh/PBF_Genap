7129edb3622f60dc08a49fff3b17ff1fb9ee6810
fauzanmuh <f4uz4n.oioi@gmail.com>
2022-03-21 00:00:02 +0700

    Modul 4 - Container Fluid

--- P 4/web-react/src/container/BlogPost/BlogPost.jsx
+++ P 4/web-react/src/container/BlogPost/BlogPost.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import './BlogPost.css';
+import Post from "../../component/BlogPost/Post";
 
 class BlogPost extends Component {
+    state = {
+        listArtikel: [],
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(jsonHasilAmbilDariAPI => {
+            this.setState( {listArtikel: jsonHasilAmbilDariAPI 
+            })
+        })
+    }
+
     render() {
         return(
-            <div class="post-artikel">
+            <div className="post-artikel">
                 <h2>Daftar Artikel</h2>
-                <div class="artikel">
-                    <div class="gambar-artikel">
-                        <img src="http://placeimg.com/80/80/tech" alt="Gambar Thumbnail Artikel" />
-                    </div>
-                    <div class="konten-artikel">
-                        <div class="judul-artikel">Judul Artikel</div>
-                        <p class="isi-artikel">Isi Artikel</p>
-                    </div>
-                </div>
+                {
+                    this.state.listArtikel.map(artikel => {
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body}/>
+                    })
+                }
             </div>
         )
     }
